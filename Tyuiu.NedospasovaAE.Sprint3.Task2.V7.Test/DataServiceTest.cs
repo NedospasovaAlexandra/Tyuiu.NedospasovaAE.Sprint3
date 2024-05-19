434627716051b7e22f512f33e0df0fa99238683a
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-19 20:47:06 +0500

    Task2

---
+++ Tyuiu.NedospasovaAE.Sprint3.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NedospasovaAE.Sprint3.Task2.V7.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint3.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            var res = ds.GetSumSeries(startValue, stopValue);
+            double wait = 81.083;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NedospasovaAE.Sprint3.Task2.V7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NedospasovaAE.Sprint3.Task2.V7.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint3.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Недоспасова.А.Э | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнила: Недоспасова Александра Эдуардовна | ИИПб-23-2                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+            Console.WriteLine("* ряда по формуле                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 1;
+            int stopValue = 10;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetSumSeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
