1357df463fab722a2e8bf1be39ac0bea6635a30e
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-20 22:41:09 +0500

    Task7

---
+++ Tyuiu.NedospasovaAE.Sprint3.Task7.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NedospasovaAE.Sprint3.Task7.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NedospasovaAE.Sprint3.Task7.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d63833fa-2538-4f63-b41e-58f5d164cc10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
